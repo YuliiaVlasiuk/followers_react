8ae93d5fd36d8b2d0bbe33f8e0532e165e04d8c3
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-04-23 22:39:35 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import { StrictMode } from "react";
+//import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./components/App";
@@ -9,9 +9,9 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
+ // <StrictMode>
     <BrowserRouter basename="/followers_react">
       <App />
     </BrowserRouter>
-</StrictMode>
+//</StrictMode>
 );

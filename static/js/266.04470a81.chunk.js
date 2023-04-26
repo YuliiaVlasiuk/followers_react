1e6dce580745fb545e3ce50a0c9391c824e2426a
YuliiaVlasiuk <110493392+YuliiaVlasiuk@users.noreply.github.com>
2023-04-26 19:27:57 +0000

    Deploying to gh-pages from @ YuliiaVlasiuk/followers_react@d75b1cbdfe4eb5f3c24f95bad3e9016c05e35362 🚀

---
+++ static/js/266.04470a81.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[266],{266:function(e,r,s){s.r(r),s.d(r,{Home:function(){return t}});var o=s(184),t=function(){return(0,o.jsx)("main",{children:(0,o.jsx)("img",{src:"https://wegeekgirls.files.wordpress.com/2014/12/we-love-our-followers.png",alt:"followers"})})};r.default=t}}]);
+//# sourceMappingURL=266.04470a81.chunk.js.map
